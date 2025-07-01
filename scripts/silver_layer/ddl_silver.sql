28c703048d489e5ca91470b7430cd5b6a7503ef5
Vikram Kumar Matte <mattevikramkumar@gmail.com>
2025-06-30 20:18:05 -0500

    Update ddl_silver.sql

--- scripts/silver_layer/ddl_silver.sql
+++ scripts/silver_layer/ddl_silver.sql
@@ -27,7 +27,7 @@ CREATE TABLE silver.crm_cust_info (
     cst_marital_status  NVARCHAR(50),
     cst_gndr            NVARCHAR(50),
     cst_create_date     DATE,
-    dwh_create_date     DATETIME2 DEFAULT GETDATE()
+    dwh_create_date     DATE DEFAULT GETDATE()
 );
 GO
 
@@ -66,7 +66,7 @@ CREATE TABLE silver.crm_sales_details (
     sls_sales           INT,
     sls_quantity        INT,
     sls_price           INT,
-    dwh_create_date     DATETIME2 DEFAULT GETDATE()
+    dwh_create_date     DATE DEFAULT GETDATE()
 );
 GO
 
@@ -84,7 +84,7 @@ CREATE TABLE silver.erp_cust_az12 (
     cid                 NVARCHAR(50),
     bdate               DATE,
     gen                 NVARCHAR(50),
-    dwh_create_date     DATETIME2 DEFAULT GETDATE()
+    dwh_create_date     DATE DEFAULT GETDATE()
 );
 GO
 
@@ -97,7 +97,7 @@ GO
 CREATE TABLE silver.erp_loc_a101 (
     cid                 NVARCHAR(50),
     cntry               NVARCHAR(50),
-    dwh_create_date     DATETIME2 DEFAULT GETDATE()
+    dwh_create_date     DATE DEFAULT GETDATE()
 );
 GO
 
@@ -112,7 +112,7 @@ CREATE TABLE silver.erp_px_cat_g1v2 (
     cat                 NVARCHAR(50),
     subcat              NVARCHAR(50),
     maintenance         NVARCHAR(50),
-    dwh_create_date     DATETIME2 DEFAULT GETDATE()
+    dwh_create_date     DATE DEFAULT GETDATE()
 );
 GO
 
